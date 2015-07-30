84a397d4b3df96d8e6584ccc83e131656c1ec21b
Marat Dyatko <marat.dyatko@klarna.com>
2015-07-30 14:17:49 +0200

    Don't enable loca in Phantom.js

--- client.js
+++ client.js
@@ -1,3 +1,17 @@
 ;(function(){
-	mocha.reporter(mocha.WebKit);
+	var isPhantom = window._phantom || window.callPhantom || navigator.userAgent.match(/PhantomJS/);
+	var OriginalReporter = mocha._reporter;
+	var WebkitReporter = mocha.WebKit;
+
+	mocha.WebKit = function (runner) {
+		if (OriginalReporter) {
+			OriginalReporter.call(this, runner);
+		}
+
+		return WebkitReporter.call(this, runner);
+	};
+
+	if (!isPhantom) {
+		mocha.reporter(mocha.WebKit);
+	}
 })();
